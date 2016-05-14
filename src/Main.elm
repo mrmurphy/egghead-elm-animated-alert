0ead515c625f621babdf868fa85430420ab0415e
Murphy Randle <murphyrandle@gmail.com>
2016-05-14 07:20:48 -0600

    End point

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,9 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.App
 import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Task
+import Process
 
 
 -- Subscriptions
@@ -20,7 +23,13 @@ subs model =
 main : Program Never
 main =
     Html.App.program
-        { init = ( {}, Cmd.none )
+        { init =
+            ( { beginShow = False
+              , show = False
+              , beginHide = False
+              }
+            , Cmd.none
+            )
         , update = update
         , view = view
         , subscriptions = subs
@@ -32,7 +41,10 @@ main =
 
 
 type alias Model =
-    {}
+    { beginShow : Bool
+    , show : Bool
+    , beginHide : Bool
+    }
 
 
 
@@ -40,7 +52,11 @@ type alias Model =
 
 
 type Msg
-    = NoOp
+    = BeginShow
+    | BeginHide
+    | Hide
+    | Show
+    | NoOp
 
 
 
@@ -50,6 +66,26 @@ type Msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        BeginShow ->
+            ( { model | beginShow = True, show = True }
+            , Task.perform (always NoOp) (always Show) (Process.sleep 1000)
+            )
+
+        BeginHide ->
+            ( { model | beginHide = True }
+            , Task.perform (always NoOp) (always Hide) (Process.sleep 1000)
+            )
+
+        Show ->
+            ( { model | show = True, beginShow = False }
+            , Task.perform (always NoOp) (always BeginHide) (Process.sleep 1000)
+            )
+
+        Hide ->
+            ( { model | show = False, beginHide = False }
+            , Cmd.none
+            )
+
         NoOp ->
             ( model, Cmd.none )
 
@@ -64,13 +100,23 @@ view model =
         [ h1 [] [ text "Animated Alert" ]
         , button
             [ class "btn btn-primary"
+            , onClick BeginShow
             ]
             [ text "Show Alert" ]
         , h1 [] [ text "Model" ]
         , p [] [ text (toString model) ]
-        , div
-            [ class "alert alert-danger"
-            ]
-            [ text ""
-            ]
+        , if model.show then
+            div
+                [ classList
+                    [ ( "alert", True )
+                    , ( "alert-danger", True )
+                    , ( "alert-danger", True )
+                    , ( "animated", True )
+                    , ( "fadeInDown", model.beginShow )
+                    , ( "fadeOutDown", model.beginHide )
+                    ]
+                ]
+                [ text "Oh no! Dinosaurs!" ]
+          else
+            span [] []
         ]
